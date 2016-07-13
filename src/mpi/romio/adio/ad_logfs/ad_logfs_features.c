25975b62d505f8c1df4290bfbb5f1fc660e8f8fc
Rob Latham <robl@mcs.anl.gov>
2016-07-13 16:00:33 -0500

    shared file pointers won't work yet
    maybe the right way to implement shared file pointers is to drop down to
    the target file system somehow.  maintaining the hidden shared file
    pointer file as a logfs log is a silly idea anyway.

--- src/mpi/romio/adio/ad_logfs/ad_logfs_features.c
+++ src/mpi/romio/adio/ad_logfs/ad_logfs_features.c
@@ -11,7 +11,6 @@ int ADIOI_LOGFS_Feature(ADIO_File fd, int flag)
 {
     switch(flag) {
 	case ADIO_LOCKS:
-	case ADIO_SHARED_FP:
 	case ADIO_DATA_SIEVING_WRITES:
 	case ADIO_UNLINK_AFTER_CLOSE:
 	case ADIO_TWO_PHASE:
@@ -20,6 +19,7 @@ int ADIOI_LOGFS_Feature(ADIO_File fd, int flag)
 	    break;
 	case ADIO_SCALABLE_OPEN:
 	case ADIO_ATOMIC_MODE:
+	case ADIO_SHARED_FP:
 	default:
 	    return 0;
 	    break;
